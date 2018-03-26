ee41ed7191110d8c35d44c80aeb3de9263a0498d
Yanda Huang <ward11sdo@qq.com>
2018-03-26 11:58:52 -0400

    Add in app tab

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
-import Grid from 'material-ui/Grid';
-import logo from './img/motor.svg';
+import { withStyles } from 'material-ui/styles';
 import './App.css';
 import CommandCenter from './CommandCenter';
 import JoyWrapper from './JoyWrapper';
 import DestinationSelector from './DestinationSelector'
-import BottomNavigation, { BottomNavigationAction } from 'material-ui/BottomNavigation';
-import ChatIcon from 'material-ui-icons/Chat';
+import AppBar from 'material-ui/AppBar';
+import Tabs, { Tab } from 'material-ui/Tabs';import ChatIcon from 'material-ui-icons/Chat';
 import OpenWithIcon from 'material-ui-icons/OpenWith';
 import LocationOnIcon from 'material-ui-icons/LocationOn';
-import PropTypes from 'prop-types';
+
+const styles = theme => ({
+  navbar: {
+    flexGrow: 1
+  },
+});
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      value: "commander",
+      value: 'commander',
     };
+    this.handleChange = this.handleChange.bind(this)
   }
 
   handleChange(event, value){
@@ -24,30 +29,25 @@ class App extends Component {
   };
 
   render() {
+    const { classes } = this.props;
     const { value } = this.state;
     return (
       <div className="App">
-        <header className="App-header" style={{position:'relative'}}>
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Cabot GUI</h1>
-        </header>
-        <CommandCenter></CommandCenter>
-        <Grid container spacing={24} justify="center">
-          <JoyWrapper></JoyWrapper>
-          <DestinationSelector></DestinationSelector>
-        </Grid>
-        <BottomNavigation value={value} onChange={this.handleChange} showLabels >
-          {/* <BottomNavigationAction label="Commander" value="commander" icon={<ChatIcon />} />
-          <BottomNavigationAction label="Joystick" value="joystick" icon={<OpenWithIcon />} />
-          <BottomNavigationAction label="Destination" value="destination" icon={<LocationOnIcon />} /> */}
-        </BottomNavigation>
+        <div className={classes.navbar}>
+          <AppBar position="static">
+            <Tabs value={value} onChange={this.handleChange}>
+              <Tab label="Commander" value="commander" icon={<ChatIcon />} />
+              <Tab label="Joystick" value="joystick" icon={<OpenWithIcon />} />
+              <Tab label="Destination" value="destination" icon={<LocationOnIcon />} />
+            </Tabs>
+          </AppBar>
+        </div>
+        {value === 'commander' && <CommandCenter/>}
+        {value === 'joystick' && <JoyWrapper/>}
+        {value === 'destination' && <DestinationSelector/>}
       </div>
     );
   }
 }
 
-App.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default App;
+export default withStyles(styles)(App);
